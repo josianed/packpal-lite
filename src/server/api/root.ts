3198cdd43cb4dd032785b52b0f78629467534cca
josianed <jsu.dufour@gmail.com>
2023-12-24 17:03:18 -0500

    create router for item; remove remaining example router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { packRouter } from "~/server/api/routers/pack";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { itemRouter } from "./routers/item";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   pack: packRouter,
+  item: itemRouter,
 });
 
 // export type definition of API
