a104aa0c5a121c1446787cac5bda866879d7ed3b
josianed <jsu.dufour@gmail.com>
2023-12-24 16:59:47 -0500

    remove usage of removed example query output

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,9 +44,7 @@ export default function Home() {
               </div>
             </Link>
           </div>
-          <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-          </p>
+          <p className="text-2xl text-white"></p>
         </div>
       </main>
     </>
